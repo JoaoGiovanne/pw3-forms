db3c6be18b91966afb4636f33ac6072edc3619a1
JoaoGiovanne <124682607+JoaoGiovanne@users.noreply.github.com>
2024-05-14 18:09:18 -0300

    Add files via upload

---
+++ src/custom-validator.ts
@@ -0,0 +1,39 @@
+import { AbstractControl, FormGroup, ValidationErrors } from "@angular/forms";
+
+export class CustomValidator {
+  static contemEspacos(control: AbstractControl){
+    if(!control.value){
+      return null;
+    }
+    let resultado = control.value.indexOf(' ') != -1;
+
+    if (resultado){
+      control.setErrors({ contemEspacos: true});
+      return { contemEspacos: true};
+    }
+    else{
+      control.setErrors(null);
+      return null;
+    }
+  }
+
+  static mesmasSenhas(senha: string, confirmaSenha: string){
+    return (formGroup: AbstractControl): ValidationErrors | null =>{
+      const senhaControl = formGroup.get(senha);
+      const confirmaSenhaControl = formGroup.get(confirmaSenha);
+
+      if (!senhaControl || !confirmaSenhaControl){
+        return null;
+      }
+
+      if (senhaControl.value !== confirmaSenhaControl.value){
+        confirmaSenhaControl.setErrors({ senhasDiferentes: true});
+        return { senhasDiferentes: true};
+      }
+      else{
+        confirmaSenhaControl.setErrors(null);
+        return null;
+      }
+    }
+  }
+}
