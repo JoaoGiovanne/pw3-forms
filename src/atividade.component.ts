062b6a440362f52ace14130576c6378b53752f48
JoaoGiovanne <124682607+JoaoGiovanne@users.noreply.github.com>
2024-05-14 18:08:46 -0300

    Add files via upload

---
+++ src/atividade.component.ts
@@ -0,0 +1,28 @@
+import { FormBuilder, FormsModule, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+import { CustomValidator } from '../validator/custom-validator';
+
+@Component({
+  selector: 'app-atividade',
+  templateUrl: './atividade.component.html',
+  styleUrls: ['./atividade.component.css']
+})
+export class AtividadeComponent {
+  ocultarSenha = true;
+
+  constructor(private formBuilder: FormBuilder){ }
+
+  cadastroForm = this.formBuilder.group({
+    nome : this.formBuilder.control('', [Validators.required,  Validators.minLength(3),  Validators.maxLength(35)]),
+    email : this.formBuilder.control('', [Validators.email, Validators.required, Validators.maxLength(40)]),
+    usuario: this.formBuilder.control('', [Validators.required, Validators.maxLength(15), CustomValidator.contemEspacos]),
+    senha: this.formBuilder.control('', [Validators.required,  Validators.pattern('^(?=.*[A-Z])(?=.*[!.@#$&*])(?=.*[0-9])(?!.* ).{8,16}$'), Validators.minLength(8),  Validators.maxLength(16)]),
+    confirmarSenha: this.formBuilder.control('', [Validators.required, CustomValidator.mesmasSenhas("senha", "confirmarSenha")]),
+  });
+
+  onEnviar(){
+    console.table(this.cadastroForm.value);
+  }
+
+
+}
